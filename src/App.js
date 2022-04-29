0f4d9e2a057943843f69d8273a86191be35a3a61
Jhonatan Cueva <jc@Jhonatans-MacBook-Pro.local>
2022-04-29 16:25:25 -0500

    7-1:fin

--- src/App.js
+++ src/App.js
@@ -5,65 +5,45 @@ import { reducer } from "./reducer";
 import "./App.css";
 
 function App() {
-  const defaultState = { people: [], isModalOpen: false, modalContent: "" };
-
-  const [name, setName] = useState("");
-  const [state, dispatch] = useReducer(reducer, defaultState);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    if (name) {
-      const newPerson = { id: new Date().getTime().toString(), name };
-      dispatch({ type: "ADD_ITEM", payload: newPerson });
-      setName("");
-    } else {
-      dispatch({ type: "NO_VALUE" });
-    }
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
   };
 
-  const closeModal = () => {
-    dispatch({ type: "CLOSE_MODAL" });
-  };
-
-  return (
-    <>
-      <div className="container">
-        {state.isModalOpen && (
-          <Modal modalContent={state.modalContent} closeModal={closeModal} />
-        )}
-
-        <form className="form" onClick={handleClick}>
-          <div>
-            <label>Name : </label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <button className="btn" type="submit">
-              Add
-            </button>
-          </div>
-        </form>
-
-        {state.people.map((person) => {
-          const { id, name } = person;
+  const List = ({ people, removePerson }) => {
+    return (
+      <div>
+        {people.map((person) => {
           return (
-            <div key={id} className="item">
-              <h4> {name}</h4>
-              <button
-                className=""
-                onClick={() => {
-                  dispatch({ type: "REMOVE_ITEM", payload: person.id });
-                }}
-              >
-                remove{" "}
-              </button>
-            </div>
+            <SinglePerson
+              key={person.id}
+              {...person}
+              removePerson={removePerson}
+            />
           );
         })}
       </div>
-    </>
+    );
+  };
+
+  const SinglePerson = ({ id, name, removePerson }) => {
+    return (
+      <div key={id} className="item">
+        <h4>{name}</h4>
+        <button onClick={() => removePerson(id)}>remove</button>
+      </div>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <h3> prop drilling</h3>
+        <List people={people} removePerson={removePerson} />
+      </section>
+    </React.Fragment>
   );
 }
 

e1852df8d7217f3cd331fd432e197bbcd35986c7
Jhonatan Cueva <jc@Jhonatans-MacBook-Pro.local>
2022-04-29 16:08:48 -0500

    6-1 : fin

--- src/App.js
+++ src/App.js
@@ -3,48 +3,46 @@ import Modal from "./Modal";
 import { data } from "./data";
 import { reducer } from "./reducer";
 import "./App.css";
-/*  */
-const defaultState = {
-  people: [],
-  isModalOpen: false,
-  modalContent: "",
-};
-/*  */
+
 function App() {
-  //
+  const defaultState = { people: [], isModalOpen: false, modalContent: "" };
+
   const [name, setName] = useState("");
   const [state, dispatch] = useReducer(reducer, defaultState);
-  //
-  const handleSubmit = (e) => {
+
+  const handleClick = (e) => {
     e.preventDefault();
     if (name) {
-      const newItem = { id: new Date().getTime().toString(), name };
-      dispatch({ type: "ADD_ITEM", payload: newItem });
+      const newPerson = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newPerson });
       setName("");
     } else {
       dispatch({ type: "NO_VALUE" });
     }
   };
-  //
+
   const closeModal = () => {
     dispatch({ type: "CLOSE_MODAL" });
   };
-  //
+
   return (
     <>
-      {state.isModalOpen && (
-        <Modal closeModal={closeModal} modalContent={state.modalContent} />
-      )}
-
       <div className="container">
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="">
+        {state.isModalOpen && (
+          <Modal modalContent={state.modalContent} closeModal={closeModal} />
+        )}
+
+        <form className="form" onClick={handleClick}>
+          <div>
+            <label>Name : </label>
             <input
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
-            <button type="submit">Add</button>
+            <button className="btn" type="submit">
+              Add
+            </button>
           </div>
         </form>
 
@@ -54,11 +52,12 @@ function App() {
             <div key={id} className="item">
               <h4> {name}</h4>
               <button
+                className=""
                 onClick={() => {
-                  dispatch({ type: "REMOVE_ITEM", payload: id });
+                  dispatch({ type: "REMOVE_ITEM", payload: person.id });
                 }}
               >
-                remove
+                remove{" "}
               </button>
             </div>
           );
